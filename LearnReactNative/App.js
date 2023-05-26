99b637289698fd36c55338cf9efecb6dfab78303
leejaekwon <nowkwon97@gmail.com>
2023-05-26 10:04:28 +0900

    ios 빌드위해 pod 설치 및 업데이트, conponenets 폴더 생성

--- LearnReactNative/App.js
+++ LearnReactNative/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { SafeAreaView,View,Text } from "react-native";
+import Greeting from './components/greeting';
 
 const App = () => {
   return(
+    // <SafeAreaView/> -> iPhoneX 이상 기종에서 디스플레이의 보이지 않는 영역 및 최하단 영역에 내용이 보여지는 것을 방지
+    // <View/> -> 가장 기본적인 컴포넌트로 레이아웃 및 스타일 담당
+    // <Text/> -> 텍스트를 보여주는 역할
     <SafeAreaView>
     <View>
-    <Text>Hello React</Text>
+    <Text>Hello React!!</Text>
+    <Greeting/>
     </View>
     </SafeAreaView>
     ); 
---
+++ LearnReactNative/components/greeting.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function Greeting() {
+  return (
+    <View>
+      <Text>테스트입니다.</Text>
+    </View>
+  )
+}
+export default Greeting;
