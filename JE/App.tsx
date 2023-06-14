e33d53173b44d093bcb6ca4a17c2a3cc0424efba
jungjieun <je_eun2735@naver.com>
2023-06-14 12:37:16 +0900

    JE 프로젝트 생성

---
+++ JE/App.tsx
@@ -0,0 +1,26 @@
+// App.tsx
+
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello, World!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
