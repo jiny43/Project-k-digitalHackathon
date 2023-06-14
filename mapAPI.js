65ce1f74dec28f7e61202342cabfbde53684980b
jungheeeun <jungheeeun272@gmail.com>
2023-06-14 11:24:13 +0900

    API 오라클빌딩  마커표시

--- mapAPI.js
+++ mapAPI.js
@@ -1,7 +1,7 @@
 import http from 'http';
 import { type } from 'os';
 import html from './html.js';
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3030
 
 const server = http.createServer((req,res)=>{
   res.writeHead(200, {'Content-type':'html'})
