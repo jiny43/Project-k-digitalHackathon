af44874707b894c0953e24d30879c7577a50238d
jungjieun <je_eun2735@naver.com>
2023-06-14 16:28:03 +0900

    AppDelegate.h 에 google API 기입

--- JE/ios/JE/AppDelegate.h
+++ JE/ios/JE/AppDelegate.h
@@ -1,3 +1,14 @@
+#import <GoogleMaps/GoogleMaps.h>
+
+@implementation AppDelegate
+...
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
++  [GMSServices provideAPIKey:@"_YOUR_API_KEY_"]; // add this line using the api key obtained from Google Console
+...
+
+
 #import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
 
