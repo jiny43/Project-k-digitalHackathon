940209cba71ec22c0b8fe82da6b455d941029630
jungjieun <je_eun2735@naver.com>
2023-06-15 13:02:06 +0900

    issus11로 돌아갈게요

--- JE/App.tsx
+++ JE/App.tsx
@@ -1,26 +1,24 @@
-// App.tsx
+import React from "react";
+import { View, Text } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
-const App = () => {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>🥹지으니🥹</Text>
-    </View>
+    <>
+      <View style={{ flex: 1 }}>
+        <MapView
+          style={{ flex: 1 }}
+          provider={PROVIDER_GOOGLE}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </View>
+    </>
   );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-});
+}
 
 export default App;
