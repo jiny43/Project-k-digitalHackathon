0aaf08286811ad0ff821330614b8ec7af8e70f91
jungjieun <je_eun2735@naver.com>
2023-06-15 11:35:23 +0900

    podfile에 google maps 의존성 입력

--- JE/ios/JE/AppDelegate.mm
+++ JE/ios/JE/AppDelegate.mm
@@ -7,6 +7,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+   [GMSServices provideAPIKey:@"AIzaSyCnCXJvaW6e6Nbnfb9ZJ0lZwkYgJwG3oyo"];
   self.moduleName = @"JE";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
