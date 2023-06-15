3ad57ae58a3c1872f0cc1442c924a547a11b94b4
leejaekwon <nowkwon97@gmail.com>
2023-06-15 10:23:39 +0900

    naver map이랑 싸움

---
+++ LearnReactNative/ios/naverMapTest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  naverMapTest
+//
+//  Created by 재권이 on 2023/06/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearnReactNative/ios/naverMapTest/naverMapTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  naverMapTestApp.swift
+//  naverMapTest
+//
+//  Created by 재권이 on 2023/06/14.
+//
+
+import SwiftUI
+
+@main
+struct naverMapTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
