4055b58051de80911074b4955b50552d347dd0ce
leejaekwon <nowkwon97@gmail.com>
2023-06-16 12:11:21 +0900

    ADD 지도위에 겹쳐지는 control section container 추가

--- JE/App.tsx
+++ JE/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import AppleMap from './componenets/AppleMap';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  controlSection: {
+    position: 'absolute',
+    top: '20%',
+    left: '2%',
+    right: '2%',
+    bottom: 0,
+    width: '95%',
+    height: '80%',
+    backgroundColor: '#FFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  overlayText: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
 
@@ -12,6 +28,10 @@ const App = () => {
   return (
     <View style={styles.container}>
       <AppleMap />
+      {/* 지도 위에서 조작이 가능한 Control Section 컨테이너 */}
+      <View style={styles.controlSection}>
+        <Text style={styles.overlayText}>컨트롤섹션 부분</Text>
+      </View>
     </View>
   );
 };
